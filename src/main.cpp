eaf935e5638b7c06402a55ccf5728f9b8731b93c
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-12-28 17:15:42 +0800

    fix: 修改wifi名称

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <arduino.h>
 #include "U8g2lib.h"
+#include "SPIFFS.h"
 
 #include "network/wifi.h"
 
@@ -12,17 +13,18 @@ U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE); 
 
 void setup() {
     Serial.begin(115200);
+    SPIFFS.begin();
     u8g2.begin();
     u8g2.enableUTF8Print();
 
     u8g2.setFont(u8g2_font_wqy12_t_gb2312);
     if (!WifiNetwork::isConfig()) {
-        u8g2.drawUTF8(0, 12, "请连接组件wifi : Link");
+        u8g2.drawUTF8(0, 12, "连接组件wifi : Keyboard");
         u8g2.drawUTF8(0, 25, "打开 10.0.3.9");
         u8g2.sendBuffer();
-        WifiNetwork::setUp("10.0.3.9");
-    } else{
-        WifiNetwork::setUp();
+        WifiNetwork::WEBStart("10.0.3.9");
+    } else {
+        WifiNetwork::Content();  // wifi连接
     }
 }
 
